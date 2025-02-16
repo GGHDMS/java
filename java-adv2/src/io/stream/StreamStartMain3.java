2a430822a836abeb4b094665abd51066f53a1d10
HeoSeokMun <tjrans303@soongsil.ac.kr>
2025-02-16 15:55:02 +0900

    스트림 시작2

---
+++ java-adv2/src/io/stream/StreamStartMain3.java
@@ -0,0 +1,25 @@
+package io.stream;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class StreamStartMain3 {
+
+    public static void main(String[] args) throws IOException {
+
+        FileOutputStream fos = new FileOutputStream("temp/hello.dat");
+
+        byte[] bytes = {65, 66, 67, 68};
+        fos.write(bytes);
+        fos.close();
+
+        FileInputStream fis = new FileInputStream("temp/hello.dat");
+
+        byte[] buffers = fis.readAllBytes();
+        System.out.println(Arrays.toString(buffers));
+
+        fis.close();
+    }
+}
